1ed15bdec9afe7fedb78f633090cd6d126c23776
vitalyvitmens <otm_@tut.by>
2024-03-16 21:54:54 +0300

    feat(scripts\redux\productsReducer.js): создает productsReducer

---
+++ scripts/redux/productsReducer.js
@@ -0,0 +1,34 @@
+import { INCREASE_QUANTITY_ACTION, DECREASE_QUANTITY_ACTION } from './action.js'
+
+export function productsReducer(state, action) {
+  switch (action.type) {
+    case INCREASE_QUANTITY_ACTION:
+      return state.map((product) => {
+        if (product.id === action.payload.id) {
+          return {
+            ...product,
+            quantity: product.quantity + 1,
+          }
+        }
+
+        return product
+      })
+
+    case DECREASE_QUANTITY_ACTION:
+      return state.map((product) => {
+        if (product.id === action.payload.id && product.quantity > 0) {
+          return {
+            ...product,
+            quantity: product.quantity - 1,
+          }
+        }
+
+        return product
+      })
+
+    default:
+      break
+  }
+
+  return state
+}
