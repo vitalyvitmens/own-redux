5e03c90fb60bc0e5eec0e848de6422d906fa8b86
vitalyvitmens <otm_@tut.by>
2024-03-17 13:13:38 +0300

    feat: заменяет custom Redux готовым

--- scripts/redux/store.js
+++ scripts/redux/store.js
@@ -1,31 +1,44 @@
+//! Раскомментрий закомментированный код и закомментируй незакомментируемый код для использования собственного custom Redux вместо стандартного Redux
+// import { productsReducer } from './productsReducer.js'
+
+// class Store {
+//   state = {}
+//   subscribers = []
+
+//   constructor(reducers) {
+//     this.reducers = reducers
+//   }
+
+//   getState() {
+//     return this.state
+//   }
+
+//   dispatch(action) {
+//     for (const reducerName in this.reducers) {
+//       const reducer = this.reducers[reducerName]
+//       const newState = reducer(this.state[reducerName], action)
+//       this.state[reducerName] = newState
+//     }
+//     this.subscribers.forEach((cb) => cb())
+//   }
+
+//   subscribe(cb) {
+//     this.subscribers.push(cb)
+//   }
+// }
+
+// export const store = new Store({
+//   products: productsReducer,
+// })
+
+import {
+  createStore,
+  combineReducers,
+} from '../../node_modules/redux/es/redux.mjs'
 import { productsReducer } from './productsReducer.js'
 
-class Store {
-  state = {}
-  subscribers = []
-
-  constructor(reducers) {
-    this.reducers = reducers
-  }
-
-  getState() {
-    return this.state
-  }
-
-  dispatch(action) {
-    for (const reducerName in this.reducers) {
-      const reducer = this.reducers[reducerName]
-      const newState = reducer(this.state[reducerName], action)
-      this.state[reducerName] = newState
-    }
-    this.subscribers.forEach((cb) => cb())
-  }
-
-  subscribe(cb) {
-    this.subscribers.push(cb)
-  }
-}
-
-export const store = new Store({
-  products: productsReducer,
-})
+export const store = new createStore(
+  combineReducers({
+    products: productsReducer,
+  })
+)
